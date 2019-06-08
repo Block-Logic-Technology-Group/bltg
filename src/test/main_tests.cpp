b2856e9a5f3528f99370eaa9d337903793d81953
Chris <chris@block-logic.com>
2019-06-07 21:43:11 -0400

    tests

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -12,41 +12,20 @@
 
 BOOST_AUTO_TEST_SUITE(main_tests)
 
-CAmount nMoneySupplyPoWEnd = 30000200 * COIN;
+CAmount nMoneySupplyEnd = 33000000 * COIN;
 
 BOOST_AUTO_TEST_CASE(subsidy_limit_test)
 {
     CAmount nSum = 0;
-    for (int nHeight = 0; nHeight < 1; nHeight += 1) {
-        /* premine in block 1 (60,001 PIV) */
+    for (int nHeight = 0; nHeight < 12; nHeight += 1) {
+        /* premine in block 1-11 (33,000,000 BLTG) */
         CAmount nSubsidy = GetBlockValue(nHeight);
-        BOOST_CHECK(nSubsidy <= 60001 * COIN);
-        nSum += nSubsidy;
-    }
-
-    for (int nHeight = 1; nHeight < 86400; nHeight += 1) {
-        /* PoW Phase One */
-        CAmount nSubsidy = GetBlockValue(nHeight);
-        BOOST_CHECK(nSubsidy <= 250 * COIN);
-        nSum += nSubsidy;
-    }
-
-    for (int nHeight = 86400; nHeight < 151200; nHeight += 1) {
-        /* PoW Phase Two */
-        CAmount nSubsidy = GetBlockValue(nHeight);
-        BOOST_CHECK(nSubsidy <= 225 * COIN);
-        nSum += nSubsidy;
-    }
-
-    for (int nHeight = 151200; nHeight < 259200; nHeight += 1) {
-        /* PoW Phase Two */
-        CAmount nSubsidy = GetBlockValue(nHeight);
-        BOOST_CHECK(nSubsidy <= 45 * COIN);
+        BOOST_CHECK(nSubsidy <= 3000000 * COIN);
         BOOST_CHECK(MoneyRange(nSubsidy));
         nSum += nSubsidy;
-        BOOST_CHECK(nSum > 0 && nSum <= nMoneySupplyPoWEnd);
-    }
-    BOOST_CHECK(nSum == 4109975100000000ULL);
+        BOOST_CHECK(nSum > 0 && nSum <= nMoneySupplyEnd);
+}
+    BOOST_CHECK(nSum == 3300000000000000ULL);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
