63fea43a0c11711da39d66c6555ef84ab72bf1b2
Chris Passarella <chris@block-logic.com>
2019-06-29 14:50:33 -0400

    test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -222,7 +222,7 @@ public:
         nMintRequiredConfirmations = 20; //the maximum amount of confirmations until accumulated in 19
         nRequiredAccumulation = 1;
         nDefaultSecurityLevel = 100; //full security level for accumulators
-        nZerocoinHeaderVersion = 3; //Block headers must be this version once zerocoin is active
+        nZerocoinHeaderVersion = 4; //Block headers must be this version once zerocoin is active
         nZerocoinRequiredStakeDepth = 200; //The required confirmations for a zbltg to be stakable
 
         nBudget_Fee_Confirmations = 6; // Number of confirmations for the finalization fee
