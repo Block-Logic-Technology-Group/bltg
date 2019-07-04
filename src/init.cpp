4f5abfa39b00bc60fe79b651ab69c72c595d9f4a
Fuzzbawls <fuzzbawls@gmail.com>
2019-07-03 22:45:35 -0700

    [Refactor] Move ThreadStakeMinter out of net.cpp
    This moves the implementation function and thread creation to a more
    appropriate file (`miner.cpp`).
    
    (cherry picked from commit 42fe4040acff08450168416ccf35413abdc1747a)

--- src/init.cpp
+++ src/init.cpp
@@ -1967,6 +1967,11 @@ bool AppInit2()
             // Run a thread to precompute any zBLTG spends
             threadGroup.create_thread(boost::bind(&ThreadPrecomputeSpends));
         }
+
+        if (GetBoolArg("-staking", true)) {
+            // ppcoin:mint proof-of-stake blocks in the background
+            threadGroup.create_thread(boost::bind(&ThreadStakeMinter));
+        }
     }
 #endif
 
