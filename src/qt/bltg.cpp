8057b75df77d2b61f0056fe2a0f89dc62a4e88d7
cpass <chris@block-logic.com>
2020-01-27 17:40:59 -0500

    More fixes for missing use statements

--- src/qt/bltg.cpp
+++ src/qt/bltg.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
-// Copyright (c) 2015-2018 The PIVX developers
+// Copyright (c) 2015-2019 The PIVX developers
 // Copyright (c) 2018-2019 The BLTG developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -588,7 +588,7 @@ int main(int argc, char* argv[])
 
 #ifdef ENABLE_WALLET
     /// 7a. parse masternode.conf
-    string strErr;
+    std::string strErr;
     if (!masternodeConfig.read(strErr)) {
         QMessageBox::critical(0, QObject::tr("BLTG Core"),
             QObject::tr("Error reading masternode configuration file: %1").arg(strErr.c_str()));
