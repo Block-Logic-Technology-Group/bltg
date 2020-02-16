5d71ac0e445338ec67324707f3a5a21b47cc9a85
cpass <chris@block-logic.com>
2020-02-15 19:09:30 -0500

    Correct block for regtest

--- src/miner.cpp
+++ src/miner.cpp
@@ -129,7 +129,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
     // -blockversion=N to test forking scenarios
     if (Params().MineBlocksOnDemand()) {
         if (fZerocoinActive)
-            pblock->nVersion = 5;
+            pblock->nVersion = 4;
         else
             pblock->nVersion = 3;
 
